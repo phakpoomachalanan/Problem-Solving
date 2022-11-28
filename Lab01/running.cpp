8445fe635f5e339fff0d8ed48321b9691c7bf330
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-11-28 22:28:49 +0700

    87 [ PPPPPP-P ]

--- Lab01/running.cpp
+++ Lab01/running.cpp
@@ -2,14 +2,33 @@
 
 int main(void)
 {
-    int    candidate, round;
+    int    i, k=0;
+    float  candidate, round, min;
     float* period;
     float  out;
     
-    std::cin >> candidate, round;
+    std::cin >> candidate >> round;
     period = (float*)malloc(sizeof(float)*candidate);
 
-    
+    std::cin >> period[0];
+    min = period[0];
+    for (i=1; i<candidate; i++)
+    {
+        std::cin >> period[i];
+        if (period[i] < min)
+        {
+            min = period[i];
+        }
+    }
+    out = min*round/(round-1);
+    for (i=0; i<candidate; i++)
+    {
+        if (period[i] < out)
+        {
+            k++;
+        }
+    }
+    std::cout << k << "\n";
 
     return 0;
 }
