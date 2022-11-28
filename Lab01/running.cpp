9a5a9cbf15ae8352a3178c2283a97ace7953f5d9
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-11-28 18:44:28 +0700

    Create running.cpp

---
+++ Lab01/running.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    int    candidate, round;
+    float* period;
+    float  out;
+    
+    std::cin >> candidate, round;
+    period = (float*)malloc(sizeof(float)*candidate);
+
+    
+
+    return 0;
+}
