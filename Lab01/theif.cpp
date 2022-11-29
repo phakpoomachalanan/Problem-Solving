03d6c14642a33f757e7e9b2cbcb58b52e4f72e08
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-11-29 09:50:07 +0700

    100 [ PPPPPPPPPP ]

--- Lab01/theif.cpp
+++ Lab01/theif.cpp
@@ -2,9 +2,30 @@
 
 int main(void)
 {
-    int n, k, t, check;
+    int  n, k, t, check=0;
+    int* path;
 
     std::cin >> n >> k >> t;
+    path = (int*)malloc(sizeof(int)*n);
+    for (int i=0; i<n; i++)
+    {
+        path[i] = 0;
+    }
+    for (int i=0; i<n; i=(i+k)%n)
+    {
+        if (i == t-1)
+        {       
+            check++;
+            break;
+        }
+        if (path[i] == 1)
+        {
+            break;
+        }
+        path[i] = 1;
+        check++;
+    }
+    std::cout << check << "\n";
 
     return 0;
 }
