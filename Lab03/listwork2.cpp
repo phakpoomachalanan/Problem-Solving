dcfb67b8625719f253d841d4e3cafd115c1b7ac7
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-19 14:53:14 +0700

    66 [ PPPP-- ]

---
+++ Lab03/listwork2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(void)
+{
+    int  n;
+    int  temp;
+    char command;
+    list<int> data;
+    list<int>::iterator it;
+
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> command >> temp;
+
+        switch (command)
+        {
+            case 'I':
+                data.push_front(temp);
+                break;
+
+            case 'A':
+                data.push_back(temp);
+                break;
+
+            case 'D':
+                if (temp <= data.size())
+                {
+                    it = data.begin();
+                    advance(it, temp-1);
+                    data.remove(*it);
+                }
+                break;
+        }
+    }
+    for (it=data.begin(); it!=data.end(); it++)
+    {
+        cout << *it << '\n';
+    }
+
+    return 0;
+}
