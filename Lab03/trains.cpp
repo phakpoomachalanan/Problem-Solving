344bb1b52796ae09f1c722f0ba95b7f3f5cae89f
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-19 15:57:30 +0700

    temp

--- Lab03/trains.cpp
+++ Lab03/trains.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <list>
+
+using namespace std;
 
 int main(void)
 {
+    int  n, i, j;
+    char command;
+    list<int>::iterator it;
+    //list<list<int>> data;
+    //list<list<int>>::iterator itit;
 
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> command >> i >> j;
+        switch (command)
+        {
+            case 'N':
+                break;
+
+            case 'M':
+                break;
+        }
+    }
 }
