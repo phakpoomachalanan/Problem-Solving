58c10d1908094194007a1152bee920305e020295
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-19 15:05:47 +0700

    50 [ PPP--- ]

--- Lab03/ic.cpp
+++ Lab03/ic.cpp
@@ -15,17 +15,33 @@ int main(void)
 
     for (int i=0; i<n; i++)
     {
-        cin >> command >> temp;
+        cin >> command;
 
         if (command == "li")
         {
-
+            cin >> temp;
+            data.push_front(temp);
         }
         else if (command == "lr")
         {
-
+            it = data.begin();
+            temp = *it;
+            data.remove(*it);
+            data.push_back(temp);
         }
-        
+        else if (command == "ri")
+        {
+            cin >> temp;
+            data.push_back(temp);
+        }
+        else if (command == "rr")
+        {
+            it = data.begin();
+            advance(it , data.size()-1);
+            temp = *it;
+            data.remove(*it);
+            data.push_front(temp);
+        }
     }
     for (it=data.begin(); it!=data.end(); it++)
     {
