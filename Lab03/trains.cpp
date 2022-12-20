cafd58f1e8135b95e4b2a7570aba003b1137298c
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-20 13:35:22 +0700

    50 [ PPxPPxxx ]

--- Lab03/trains.cpp
+++ Lab03/trains.cpp
@@ -5,24 +5,37 @@ using namespace std;
 
 int main(void)
 {
-    int  n, i, j;
+    int  n, k, j;
     char command;
     list<int>::iterator it;
-    //list<list<int>> data;
-    //list<list<int>>::iterator itit;
+    list<int> data[100000];
 
     cin >> n;
 
     for (int i=0; i<n; i++)
     {
-        cin >> command >> i >> j;
+        cin >> command >> k >> j;
         switch (command)
         {
             case 'N':
+                data[j].push_back(k);
                 break;
 
             case 'M':
+                data[j].splice(data[j].end(), data[k]);
                 break;
         }
     }
+    for (int i=0; i<n; i++)
+    {
+        for (it=data[i].begin(); it!=data[i].end(); it++)
+        {
+            if (*it != 0)
+            {
+                cout << *it << '\n';
+            }
+        }
+    }
+
+    return 0;
 }
