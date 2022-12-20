996006ffb25e202ae276ee37146a3cd706de1ab9
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-20 21:46:30 +0700

    pass 1st test case

--- Lab03/necklace.cpp
+++ Lab03/necklace.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main(void)
 {
-    int n, i, head, tail, j;
+    int n, i, head, tail, j, flag=0;
     int head_line, tail_line;
     list<int> lace[300001];
     list<int>::iterator itr, head_itr, tail_itr;
 
     cin >> n;
-    //lace = (list<int>*)malloc(sizeof(list<int>)*n);
 
     for (i=0; i<n; i++)
     {
@@ -19,20 +18,22 @@ int main(void)
     }
     for (i=0; i<n-1; i++)
     {
-        cin >> head, tail;
+        cin >> head >> tail;
         for (j=0; j<n; j++)
         {
             for (itr=lace[j].begin(); itr!=lace[j].end(); itr++)
             {
                 if (*itr == tail)
                 {
+                    flag = 1;
                     tail_itr = itr;
                     break;
                 }
             }
-            if (tail_itr != lace[j].end())
+            if (tail_itr != lace[j].end() && flag == 1)
             {
                 tail_line = j;
+                flag = 0;
                 break;
             }
         }
@@ -42,19 +43,21 @@ int main(void)
             {
                 if (*itr == head)
                 {
+                    flag = 1;
                     head_itr = itr;
                     break;
                 }
             }
-            if (head_itr != lace[j].end())
+            if (head_itr != lace[j].end() && flag == 1)
             {
                 head_line = j;
+                flag = 0;
                 break;
             }
         }
-        lace[tail_line].splice(tail_itr, lace[head_line]);
+        lace[tail_line].splice(++tail_itr, lace[head_line]);
     }
-    for (i=0; i<n-1; i++)
+    for (i=0; i<n; i++)
     {
         if (lace[i].size() != 0)
         {
