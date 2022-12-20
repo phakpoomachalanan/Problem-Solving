a68e6b37a87981110f4c497230b5bf312a2e817d
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-20 15:59:09 +0700

    still working on it

--- Lab03/necklace.cpp
+++ Lab03/necklace.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main(void)
 {
+    int n, i, head, tail;
+    list<int>* lace;
+    list<int>::iterator itr;
 
+    cin >> n;
+    lace = (list<int>*)malloc(sizeof(list<int>)*n);
+
+    for (i=0; i<n; i++)
+    {
+        lace[i].push_back(i+1);
+    }
+    for (i=0; i<n-1; i++)
+    {
+        cin >> head, tail;
+        itr = find(lace[0].begin(), lace[0].end(), tail);
+    }
 
     return 0;
 }
