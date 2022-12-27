5f384e8bcf4f9caec13f2f7e5dc829d8c8e43a71
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2022-12-27 14:11:10 +0700

    100 [ PPPPPPP ]

---
+++ Lab04/conncount.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX_N = 100001;
+
+vector<int> adj[MAX_N];
+bool visited[MAX_N];
+int layer[MAX_N];
+int deg[MAX_N];
+int n, m;
+
+void get_input();
+void init();
+void bfs(int s);
+
+int main(void)
+{
+    int count = 0;
+
+    get_input();
+    init();
+    
+    for (int i=0; i<m; i++)
+    {
+        if (layer[i] == -1)
+        {
+            bfs(i);
+            count++;
+        }
+    }
+
+    cout << count << '\n';
+
+    return 0;
+}
+
+void get_input()
+{
+    int i;
+    int u, v;
+
+    cin >> m >> n;
+
+    for (i=0; i<m; i++)
+    {
+        deg[i] = 0;
+    }
+    for (i=0; i<n; i++)
+    {
+        cin >> u >> v;
+        u--;
+        v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        deg[u]++;
+        deg[v]++;
+    }
+}
+
+void init()
+{
+    for (int i=0; i<m; i++)
+    {
+        visited[i] = false;
+        layer[i] = -1;
+    }
+}
+
+void bfs(int s)
+{
+    vector<int> current_layer;
+    vector<int> next_layer;
+    int u, v, i;
+
+    current_layer.push_back(s);
+    visited[s] = true;
+    layer[s] = 0;
+
+    while (true)
+    {
+        for (vector<int>::iterator itr=current_layer.begin(); itr!=current_layer.end(); itr++)
+        {
+            u = *itr;
+            for (i=0; i<deg[u]; i++)
+            {
+                v = adj[u][i];
+                if (!visited[v])
+                {
+                    next_layer.push_back(v);
+                    visited[v] = true;
+                    layer[v] = layer[u] + 1;
+                }
+            }
+        }
+        if (next_layer.size() == 0)
+        {
+            break;
+        }
+        current_layer = next_layer;
+        next_layer.clear();
+    }
+}
