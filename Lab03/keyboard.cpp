f97204e9297a0d64a1c7f38d4edbeaa6c234fcc4
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-01-03 13:30:26 +0700

    100 [ uva-graded(Accepted):28139498 ]

--- Lab03/keyboard.cpp
+++ Lab03/keyboard.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
 #include <list>
-#include <stack>
 
 using namespace std;
 
 int main(void)
 {
     int  mode = 0;
+    int  flag;
     char ch;
     list<char> temp;
     list<char> text;
     list<char>::iterator itr;
 
-    do
+    while ((ch=getchar()) != EOF)
     {
-        ch = getchar();
-        if (ch == '[')
+        flag = 0;
+        if (mode == 0 && ch == '[')
         {
             mode = 1;
+        }
+        else if (mode == 1 && ch == '[')
+        {
+            text.splice(text.begin(), temp);
+            temp.clear();
         }
         else if (ch == ']')
         {
             mode = 0;
             text.splice(text.begin(), temp);
+            temp.clear();
         }
         else if (ch == '\n')
         {
             mode = 0;
             text.splice(text.begin(), temp);
             text.push_back('\n');
+            for (itr=text.begin(); itr!=text.end(); itr++)
+            {
+                cout << *itr;
+            }
+            text.clear();
+            temp.clear();
+            flag = 1;
         }
         else if (mode == 0)
         {
@@ -39,15 +52,16 @@ int main(void)
             temp.push_back(ch);
         }
     }
-    while (ch != EOF);
     text.splice(text.begin(), temp);
-    text.push_back('\0');
 
     for (itr=text.begin(); itr!=text.end(); itr++)
     {
         cout << *itr;
     }
-    cout << '\n';
+    if (flag == 0)
+    {
+        cout << '\n';
+    }
 
     return 0;
 }
