5436ea002795187c78cf5b33ab020060f23b2611
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-01-05 04:38:51 +0700

    100 [ PPPPPPP ]

--- Lab04/qualitymap.cpp
+++ Lab04/qualitymap.cpp
@@ -3,18 +3,22 @@
 
 using namespace std;
 
-int n, m;
+int n, m, block;
 int best_q=0, good_q=0;
+bool flag;
 const int MAX_N = 31;
 const int MAX_M = 31;
 int map[MAX_N][MAX_M] = {0};
 bool visited[MAX_N][MAX_M] = {false};
 
+void find_quality(int y, int x, int spe);
+
 int main(void)
 {
-    int  i, j;
+    int  i, j, temp;
     char ch;
     string row;
+    vector<pair<int, int> > coor;
 
     cin >> n >> m;
     getline(cin, row);
@@ -32,23 +36,67 @@ int main(void)
             else if (ch == '$')
             {
                 map[i][j] = 2;
+                coor.push_back(make_pair(i, j));
             }
             else if (ch == '*')
             {
                 map[i][j] = 3;
+                coor.push_back(make_pair(i, j));
             }
         }
     }
 
-    /*for (i=0; i<n; i++)
+    for (i=0; i<coor.size(); i++)
     {
-        for (j=0; j<m; j++)
+        if (!visited[coor[i].first][coor[i].second])
         {
-            cout << map[i][j];
+            block = 0;
+            flag = false;
+            
+            find_quality(coor[i].first, coor[i].second, map[coor[i].first][coor[i].second]);
+
+            if (flag)
+            {
+                best_q += block;
+            }
+            else
+            {
+                good_q += block;
+            }
         }
-        cout << '\n';
     }
-    */
+
+    cout << best_q << ' ' << good_q << '\n';
 
     return 0;
+}
+
+void find_quality(int y, int x, int spe)
+{
+    if (y>=n || y<0 || x>=m || x<0)
+    {
+        return;
+    }
+    if (map[y][x] == 0 || visited[y][x])
+    {
+        return;
+    }
+
+    visited[y][x] = true;
+    if (spe == 2 && map[y][x] == 3)
+    {
+        flag = true;
+    }
+    else if (spe == 3 && map[y][x] == 2)
+    {
+        flag = true;
+    }
+    block++;
+
+    find_quality(y+1, x, spe);
+    find_quality(y-1, x, spe);
+    find_quality(y, x+1, spe);
+    find_quality(y, x-1, spe);
+
+    return;
 }
