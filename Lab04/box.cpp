d5ad78db8f3d5b0e43fbb03b932f529c04d41fd9
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-01-05 05:03:27 +0700

    100 [ PPPPPPPPPPPP ]

---
+++ Lab04/box.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int  n, m;
+bool can;
+const int MAX_N = 31;
+const int MAX_M = 31;
+int map[MAX_N][MAX_M] = {0};
+bool visited[MAX_N][MAX_M] = {false};
+
+void find_exit(int y, int x);
+
+int main(void)
+{
+    int  i, j;
+    char ch;
+    string row;
+    vector<pair<int, int> > coor;
+
+    cin >> n >> m;
+    getline(cin, row);
+
+    for (i=0; i<n; i++)
+    {
+        getline(cin, row);
+        for (j=0; j<m; j++)
+        {
+            ch = row[j];
+            if (ch == '.')
+            {
+                map[i][j] = 1;
+            }
+        }
+    }
+
+    for (i=0; i<n; i++)
+    {
+        if (!visited[0][i])
+        {
+            find_exit(0, i);
+        }
+    }
+
+    if (can)
+    {
+        cout << "yes\n";
+    }
+    else
+    {
+        cout << "no\n";
+    }
+
+    return 0;
+}
+
+void find_exit(int y, int x)
+{
+    if (y>=n-1 || y<0 || x>=m-1 || x<0)
+    {
+        return;
+    }
+    if (map[y][x] == 0 || visited[y][x])
+    {
+        return;
+    }
+    if (y == n-2 && map[y+1][x] == 1 && map[y+1][x+1] == 1 && map[y+1][x] == 1)
+    {
+        can = true;
+        return;
+    }
+
+    visited[y][x] = true;
+    
+    if (map[y][x+1] == 1 && map[y+1][x] == 1 && map[y+1][x+1])
+    {
+        find_exit(y+1, x);
+        find_exit(y-1, x);
+        find_exit(y, x+1);
+        find_exit(y, x-1);
+    }
+
+    return;
+}
