d73ad83e995da80331e05477cd733481b674123c
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-01-31 14:26:21 +0700

    0 [ compilation error ]

--- exam_mid/countries.cpp
+++ exam_mid/countries.cpp
@@ -8,20 +8,21 @@ int des_y, des_x;
 int sta_y, sta_x;
 int spe_y, spe_x;
 const int MAX_N = 51;
-int min_dot = 0xfffffff;
-int layer[MAX_N][MAX_N];
+int min_visa = 0xfffffff;
 bool can, all_can=false;
 int map[MAX_N][MAX_N] = {0};
-vector<pair<int, int> > speacial;
-bool visited[MAX_N][MAX_N] = {false};
+bool visited[MAX_N] = {false};
+vector<int> adj[MAX_N];
+bool visited[MAX_N];
+int layer[MAX_N];
+int deg[MAX_N];
 
 void init();
-void pre_travel(int mode);
-void traversal(int y, int x, int dir, int tick);
+void bfs(int s);
 
 int main(void)
 {
-    int i, j;
+    int i, j, u, v, w;
 
     cin >> n >> m;
     cin >> sta_y >> sta_x >> des_y >> des_x;
@@ -35,7 +36,34 @@ int main(void)
         }
     }
 
-    cout << layer[des_y][des_x] << '\n';
+    for (i=0; i<n; i++)
+    {
+        for (j=0; j<m-1; j++)
+        {
+            u = map[i][j];
+            v = map[i][j+1];
+            if (u != v)
+            {
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+                deg[u]++;
+                deg[v]++;
+            }
+            if (i != n-1)
+            {
+                w = map[i+1][j];
+                if (u != w)
+                {
+                    adj[u].push_back(w);
+                    adj[w].push_back(u);
+                    deg[u]++;
+                    deg[w]++;
+                }
+            }
+        }
+    }
+
+    cout << layer[map[des_y][des_x]] << '\n';
 
     return 0;
 }
@@ -45,50 +73,45 @@ void init()
     int i, j;
 
     can = false;
-    for (i=0; i<n; i++)
+    for (j=0; j<m; j++)
     {
-        for (j=0; j<m; j++)
-        {
-            visited[i][j] = false;
-            layer[i][j] = 0xffff;
-        }
+        visited[j] = false;
     }
-    layer[des_y][des_x] = 0xffff;
 
     return;
 }
 
-void traversal(int y, int x, int last_layer)
+void bfs(int s)
 {
-    int curr_layer;
+    vector<int> current_layer;
+    vector<int> next_layer;
+    int u, v, i;
 
-    if (y>n-1 || y<0 || x>m-1 || x<0)
+    current_layer.push_back(s);
+    visited[s] = true;
+    layer[s] = 0;
+
+    while (true)
     {
-        return;
+        for (vector<int>::iterator itr=current_layer.begin(); itr!=current_layer.end(); itr++)
+        {
+            u = *itr;
+            for (i=0; i<deg[u]; i++)
+            {
+                v = adj[u][i];
+                if (!visited[v])
+                {
+                    next_layer.push_back(v);
+                    visited[v] = true;
+                    layer[v] = layer[u] + 1;
+                }
+            }
+        }
+        if (next_layer.size() == 0)
+        {
+            break;
+        }
+        current_layer = next_layer;
+        next_layer.clear();
     }
-    if (visited[y][x])
-    {
-        return;
-    }
-
-    visited[y][x] = true;
-    curr_layer = layer[y][x];
-    
-    if (curr_layer != last_layer)
-    {
-        layer[y][x]++;
-    }
-
-    if (y==des_y && x==des_x)
-    {
-        can = true;
-        return;
-    }
-
-    traversal(y+1, x, curr_layer);
-    traversal(y-1, x, curr_layer);
-    traversal(y, x-1, curr_layer);
-    traversal(y, x+1, curr_layer);
-
-    return;
 }
