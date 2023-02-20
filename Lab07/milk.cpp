2c3036140b46f1165a540d4b126eaa075b71a039
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-02-20 12:43:30 +0700

    f

---
+++ Lab07/milk.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX_N = 100001;
+const int MAX_M = 200001;
+
+int n, m;
+int parent[MAX_N];
+int p_size[MAX_N];
+int edge[MAX_M][2];
+
+void init();
+int find(int u);
+void union_tree(int u, int v);
+
+int main(void)
+{
+    int u, v, i;
+    char command;
+
+    cin >> n >> m;
+
+    for (i=0; i<m; i++)
+    {
+        cin >> command >> u >> v;
+
+        switch (command)
+        {
+            case 'c':
+                union_tree(u, v);
+                break;
+            case 'q':
+                if (find(u) == find(v))
+                {
+                    cout << "yes\n";
+                }
+                else
+                {
+                    cout << "no\n";
+                }
+
+        }
+    }
+
+    return 0;
+}
+
+void init()
+{
+    for (int i=0; i<n; i++)
+    {
+        parent[i] = i;
+        p_size[i] = 1;
+    }
+}
+
+int find(int u)
+{
+    if (parent[u] == u)
+    {
+        return u;
+    }
+    parent[u] = find(parent[u]);
+
+    return parent[u];
+}
+
+void union_tree(int u, int v)
+{
+    int temp;
+
+    v = find(v);
+    u = find(u);
+    if (p_size[u] > p_size[v])
+    {
+        temp = u;
+        u = v;
+        v = temp;
+    }
+    parent[u] = v;
+    p_size[v] += p_size[u];
+}
