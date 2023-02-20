cc939c39ea4782d84922ea71f73ea407ff5c6bd7
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-02-20 14:45:20 +0700

    100 [ PPPPPPPPPP ]

--- Lab08/probe1d.cpp
+++ Lab08/probe1d.cpp
@@ -1,19 +1,33 @@
 #include "probelib.h"
-#include <cstdio>
+#include <iostream>
 
-int n;
+using namespace std;
+
+int probe_search(int left, int right);
 
 int main(void)
 {
     int n = probe_init();
 
-    for(int i=0; i<10; i++) {
-        if(probe_check(i,i)) {
-        probe_answer(i);
-        }
-    }
-    
-    probe_answer(n-1);
+    probe_answer(probe_search(0, n-1));
 
     return 0;
+}
+
+int probe_search(int left, int right)
+{
+    int mid;
+
+    if (left == right)
+    {
+        return left;
+    }
+
+    mid = left + (right-left)/2;
+
+    if (probe_check(left, mid))
+    {
+        return probe_search(left, mid);
+    }
+    return probe_search(mid+1, right);
 }
