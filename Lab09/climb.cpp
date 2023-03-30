dd0c42a434d869f0e95028bf3294d25733fd7882
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-03-30 14:39:58 +0700

    idk

--- Lab09/climb.cpp
+++ Lab09/climb.cpp
@@ -3,121 +3,122 @@
 
 using namespace std;
 
+int spe_y, spe_x;
 int  n, m;
+bool can;
 const int MAX_N = 51;
-int min_dot = 0xfffffff;
+bool map[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N] = {false};
+vector<pair<int, int> > speacial;
 int layer[MAX_N][MAX_N];
-bool can, all_can=false;
-bool map[MAX_N][MAX_N] = {'\0'};
-bool visited[50][50];
+int des_y, des_x;
 
+void get_input();
 void init();
-void traversal(int y, int x, int dir, int tick);
+void traversal(int y, int x, int tick);
 
 int main(void)
 {
+    int min = 0xfffffff;
     int i, j;
-    char temp;
+
+    traversal(n-1, 0, 0);
+
+    if (can)
+    {
+        cout << min << '\n';
+    }
+    else
+    {
+        cout << -1 << '\n';
+    }
+
+    return 0;
+}
+
+void get_input()
+{
+    int i, j, temp;
 
     cin >> n >> m;
 
+    init();
+    
     for (i=0; i<n; i++)
     {
         for (j=0; j<m; j++)
         {
             cin >> temp;
-            map[i][j] = temp=='X';
+            map[i][j] = (temp=='X');
         }
     }
-
-    init();
-    traversal(n-1, 0, 0, 0);
-
-    if (all_can)
-    {
-        cout << layer[0][m-1] << '\n';
-    }
-    else
-    {
-        cout << "-1\n";
-    }
-
-    return 0;
 }
 
 void init()
 {
     int i, j;
 
+    can = false;
     for (i=0; i<n; i++)
     {
         for (j=0; j<m; j++)
         {
+            visited[i][j] = false;
             layer[i][j] = 0xffff;
+            map[i][j] = 0;
         }
     }
+
+    des_x = m-1;
+    des_y = 0;
 
     return;
 }
 
-void traversal(int y, int x, int plus, int tick)
+void traversal(int y, int x, int tick)
 {
-    int curr_layer;
-
-    if (y>n-1 || y<0 || x>m-1 || x<0)
+    if (y>n-2 || y<1 || x>m-2 || x<1)
     {
         return;
     }
-
     if (map[y][x] || visited[y][x])
     {
         return;
     }
 
     visited[y][x] = true;
-    curr_layer = layer[y][x];
+    // if (tick < layer[y][x])
+    // {
+    //     layer[y][x] = tick + 1;
+    // }
+    // if (layer[y+1][x] <= layer[y][x])
+    // {
+    //     layer[y][x] = layer[y+1][x] + 1;
+    // }
+    // if (layer[y-1][x] <= layer[y][x])
+    // {
+    //     layer[y][x] = layer[y-1][x] + 1;
+    // }
+    // if (layer[y][x+1] <= layer[y][x])
+    // {
+    //     layer[y][x] = layer[y][x+1] + 1;
+    // }
+    // if (layer[y][x-1] <= layer[y][x])
+    // {
+    //     layer[y][x] = layer[y][x-1] + 1;
+    // }
 
-    
-    if (tick < curr_layer)
+    if (y==des_y && x==des_x)
     {
-        curr_layer = tick + plus;
-    }
-    // if (layer[y+1][x] <= curr_layer)
-    // {
-    //     curr_layer = layer[y+1][x];
-    // }
-    // if (layer[y-1][x] <= curr_layer)
-    // {
-    //     curr_layer = layer[y-1][x] + 5;
-    // }
-    // if (layer[y][x+1] <= curr_layer)
-    // {
-    //     curr_layer = layer[y][x+1] + 1;
-    // }
-    // if (layer[y][x-1] <= curr_layer)
-    // {
-    //     curr_layer = layer[y][x-1] + 1;
-    // }
-    layer[y][x] = curr_layer;
-
-    // cout << y << " " << x << " " << layer[y][x] << " " << tick << endl;
-
-    if (y==0 && x==m-1)
-    {
+        // cout << tick << '\n';
         can = true;
-        all_can = true;
-        // cout << y << " " << x << " " << layer[y][x] << " " << tick << endl;
-        if (curr_layer > tick)
-        {
-            layer[0][m-1] = tick+plus;
-        }
         return;
     }
 
-    traversal(y+1, x, 0, curr_layer);
-    traversal(y, x-1, 1, curr_layer);
-    traversal(y, x+1, 1, curr_layer);
-    traversal(y-1, x, 5, curr_layer);
+    traversal(y+1, x, layer[y][x]);
+    traversal(y-1, x, layer[y][x]);
+    traversal(y, x-1, layer[y][x]);
+    traversal(y, x+1, layer[y][x]);
 
     return;
 }
